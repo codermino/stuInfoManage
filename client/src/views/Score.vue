40b96a419afc9d04e58810fbe61f9af9bd21ef49
mino <1227657064@qq.com>
2019-11-20 17:45:45 +0800

    12-模糊查询完成

--- client/src/views/Score.vue
+++ client/src/views/Score.vue
@@ -11,9 +11,28 @@
 
     </div>
     <div class="table_container">
+
+      <div class="search">
+        <div style="display: inline-block"> 搜索：</div>
+        <el-input
+          class="search_input"
+          v-model="search"
+          size="mini"
+          placeholder="输入关键字搜索"/>
+      </div>
+
+<!--      <el-table-->
+<!--        v-if="tableData.length > 0"-->
+<!--        :data='tableData'-->
+<!--        max-height="450"-->
+<!--        border-->
+<!--        style="width: 100%"-->
+<!--        :summary-method="getSummaries"-->
+<!--        :show-summary="userIdentity">-->
+
       <el-table
         v-if="tableData.length > 0"
-        :data='tableData'
+        :data='tables'
         max-height="450"
         border
         style="width: 100%"
@@ -142,6 +161,28 @@
       },
       userIdentity(){
         return this.$store.getters.user.identity === 'student';
+      },
+      tables () {
+        const search = this.search;
+        if (search) {
+          // filter() 方法创建一个新的数组，新数组中的元素是通过检查指定数组中符合条件的所有元素。
+          // 注意： filter() 不会对空数组进行检测。
+          // 注意： filter() 不会改变原始数组。
+          return this.tableData.filter(data => {
+            // some() 方法用于检测数组中的元素是否满足指定条件;
+            // some() 方法会依次执行数组的每个元素：
+            // 如果有一个元素满足条件，则表达式返回true , 剩余的元素不会再执行检测;
+            // 如果没有满足条件的元素，则返回false。
+            // 注意： some() 不会对空数组进行检测。
+            // 注意： some() 不会改变原始数组。
+            return Object.keys(data).some(key => {
+              // indexOf() 返回某个指定的字符在某个字符串中首次出现的位置，如果没有找到就返回-1；
+              // 该方法对大小写敏感！所以之前需要toLowerCase()方法将所有查询到内容变为小写。
+              return String(data[key]).toLowerCase().indexOf(search) > -1
+            })
+          })
+        }
+        return this.tableData;
       }
     },
     created() {
@@ -221,7 +262,18 @@
   }
   .btnRight {
     float: right;
+    margin-top: 5px;
   }
+
+  .search{
+    float: right;
+    margin:-13px 20px 0 0;
+  }
+
+  .search_input{
+    margin-top: 10px;
+  }
+
   .pagination {
     text-align: right;
     margin-top: 10px;
