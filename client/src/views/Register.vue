c23a8e53640ba96eec892d6b3f0942ff68fcdfd7
mino <1227657064@qq.com>
2020-03-15 22:54:55 +0800

    22-密码强度

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -13,11 +13,26 @@
           <el-form-item label="密码" prop="password">
             <el-input type="password" v-model="ruleForm.password" placeholder="请输入密码"></el-input>
           </el-form-item>
+
+          <div style="margin-left: 60px;margin-bottom:5px;margin-top: -20px;">
+            <div class="input_span">
+              <!--            <label>强度:</label>-->
+              <span id="one"></span>
+              <span id="two"></span>
+              <span id="three"></span>
+            </div>
+            <div id="font">
+              <span>弱</span>
+              <span>中</span>
+              <span>强</span>
+            </div>
+          </div>
+
           <el-form-item label="确认密码" prop="checkPass">
             <el-input type="password" v-model="ruleForm.checkPass" placeholder="请确认密码"></el-input>
           </el-form-item>
           <el-form-item label="选择身份" prop="identity">
-            <el-select v-model="ruleForm.identity" placeholder="请选择身份">
+            <el-select v-model="ruleForm.identity" placeholder="请选择身份" value="identity">
               <el-option label="管理员" value="manager"></el-option>
               <el-option label="学生" value="student"></el-option>
             </el-select>
@@ -40,6 +55,31 @@
 <script>
   export default {
     name: "Register",
+    computed: {
+      password() {
+        return this.ruleForm.password;
+      }
+    },
+    watch:{
+      password(newname, oldname) {
+        this.msgText = this.checkStrong(newname);
+        if (this.msgText > 1 || this.msgText === 1) {
+          document.getElementById("one").style.background = "red";
+        } else {
+          document.getElementById("one").style.background = "#eee";
+        }
+        if (this.msgText > 2 || this.msgText === 2) {
+          document.getElementById("two").style.background = "orange";
+        } else {
+          document.getElementById("two").style.background = "#eee";
+        }
+        if (this.msgText === 4) {
+          document.getElementById("three").style.background = "#00D1B2";
+        } else {
+          document.getElementById("three").style.background = "#eee";
+        }
+      }
+    },
     data() {
       let checkNumber = (rule, value, callback) => {
         if (!value) {
@@ -78,6 +118,8 @@
         }
       };
       return {
+        msgText: "",
+        pwdee: "",
         ruleForm: {
           name:'',
           userId:'',
@@ -124,6 +166,31 @@
       },
       resetForm(formName) {
         this.$refs[formName].resetFields();
+      },
+      //密码强度检验工具
+      checkStrong(sValue) {
+        let modes = 0;
+        //正则表达式验证符合要求的
+        if (sValue.length < 1) return modes;
+        if (/\d/.test(sValue)) modes++; //数字
+        if (/[a-z]/.test(sValue)) modes++; //小写
+        if (/[A-Z]/.test(sValue)) modes++; //大写
+        if (/\W/.test(sValue)) modes++; //特殊字符
+
+        //逻辑处理
+        switch (modes) {
+          case 1:
+            return 1;
+            break;
+          case 2:
+            return 2;
+            break;
+          case 3:
+          case 4:
+            return sValue.length < 6 ? 3 : 4;
+            break;
+        }
+        return modes;
       }
     }
   }
@@ -168,6 +235,47 @@
   }
   .tiparea p a {
     color: #409eff;
+  }
+
+  .input_span span {
+    display: inline-block;
+    width: 75px;
+    height: 10px;
+    background: #eee;
+    line-height: 20px;
+  }
+
+  #one {
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    border-right: 0px solid;
+    margin-left: 20px;
+    margin-right: 3px;
+  }
+
+  #two {
+    border-left: 0px solid;
+    border-right: 0px solid;
+    margin-left: -5px;
+    margin-right: 3px;
+  }
+
+  #three {
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    border-left: 0px solid;
+    margin-left: -5px;
+  }
+  #font span:nth-child(1) {
+    color: red;
+    margin-left: 80px;
+  }
+  #font span:nth-child(2) {
+    color: orange;
+    margin: 0 60px;
+  }
+  #font span:nth-child(3) {
+    color: #00d1b2;
   }
 
 </style>
